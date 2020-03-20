8dfa2c05e01db93e18b99b324f3c918d12d7d9c4
tcmyxc <1282494272@qq.com>
2020-03-20 16:44:50 +0800

    # 决定放弃VUE框架，只用jQuery

--- public/scripts/base.js
+++ public/scripts/base.js
@@ -4,14 +4,6 @@
     } else if ($("#questionnaireDesc").val() == "") {
         alert("问卷描述不能为空！");
     } else {
-        document.getElementById("modifyInfo").submit();
-    }
-};
-
-function verifyQuestionDesc() {
-    if ($("#subQuestionDesc").val() == "") {
-        alert("问题描述不能为空！");
-    } else {
-        document.getElementById("addQuestionForm").submit();
+        $("#modifyInfo").submit();
     }
 };
